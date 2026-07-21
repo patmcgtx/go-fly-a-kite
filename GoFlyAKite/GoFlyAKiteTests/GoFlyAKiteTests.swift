160f51499205fe682bb7cf92b80ced4e48e9db18
patmcgtx <57506933+patmcgtx@users.noreply.github.com>
2026-07-20 21:23:21 -0500

    Initial project skeleton

---
+++ GoFlyAKite/GoFlyAKiteTests/GoFlyAKiteTests.swift
@@ -0,0 +1,18 @@
+//
+//  GoFlyAKiteTests.swift
+//  GoFlyAKiteTests
+//
+//  Created by Patrick McGonigle on 7/20/26.
+//
+
+import Testing
+
+struct GoFlyAKiteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
